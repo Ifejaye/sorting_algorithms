0b6eefed2213d02fe53bee33b6f6f8edfe0b7af3
Ifejaye <iamifejaye@example.com>
2024-01-22 19:28:20 +0100

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array according to selection sort.
+ *
+ *@array: the array that i want to sort
+ *@size: the length of the array to be sorted
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, tmp;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+				if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+				tmp = array[i];
+				array[i] = array[min];
+				array[min] = tmp;
+
+				print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define LISTINT_H
 
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
